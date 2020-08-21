51a325250605e217ed1d88ae5c73a6bb33aeb43f
AnjanaDelhi <46753092+AnjanaDelhi@users.noreply.github.com>
2020-08-20 23:11:31 -0500

    Added net total calculation

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,13 @@ import csv
 # Get the Relative path (get by right clicking on the file)- PyBank\Resources\budget_data.csv. Then load the file path as a variable. 
 file_path = os.path.join("PyBank", "Resources", "budget_data.csv")
 
+#Creating variables needed
+total_months = 0
+net_total = 0
+average_change = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 99999999999999999]
+
 # Read the file using csv module and storing the contents as "csvfile"
 with open(file_path) as csvfile:
     file_1 = csv.reader(csvfile, delimiter= ',') #csv.reader function needs the variable and delimiter.
@@ -14,23 +21,15 @@ with open(file_path) as csvfile:
     print(f"CSV Header: {file_1_header}")
 
     # Loop for total months 
-    total_months = 0
-    net_total = 0
-    average_change = []
-    greatest_increase = ["", 0]
-    greatest_decrease = ["", 99999999999999999]
-
-
     for row in file_1:
         total_months = total_months + 1 
+        net_total = net_total + int(row[1])
+        average_change = net_total/ len(row[1])
+
         
     print("Financial Analysis")
     print("-------------------------")
     print(f"Total Months: {total_months}")
-
-        #print(f"Total: ${net_total}")
-        #print(f"Average Change: $(round(sum{net_total} / len{net_total},2)))
-
-        # print("Greatest Increase: " + str(greatest_increase[0]) + " ($" +  str(greatest_increase[1]) + ")") 
-        # print("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" +  str(greatest_decrease[1]) + ")")
-
+    print(f"Total: {net_total}")
+    print(f"Average change: {average_change}")
+       
